cb66bcec761525714fd32e08acfc295a486a9fe9
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-13 19:42:52 +0530

    Added the main screen of the flutter internals

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/keys/keys.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const Keys(),
+      ),
+    );
   }
 }
